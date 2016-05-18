4ecd1cc8622451c125879083981454ce24860e08
Aero Wong <aerowongmanhon@gmail.com>
2016-05-19 07:48:04 +0800

    added remote method for add-recommendation function

--- common/models/recommendation.js
+++ common/models/recommendation.js
@@ -1,3 +1,15 @@
 module.exports = function(Recommendation) {
-
+    Recommendation.addRecommendation = function (recommendationDetailObj, cb) {
+        var book = recommendationDetailObj.book_id,
+            egghead = recommendationDetailObj.egghead_id,
+            src = recommendationDetailObj.src,
+            recommendationObjectToSave = {};
+    }
+    Recommendation.remoteMethod('addRecommendation', {
+        description: 'Add a new book recommendation',
+        http: {path: '/addRecommendation', verb: 'post', status: 200},
+        accessType: 'WRITE',
+        accepts: {arg: 'Recommendation info', type: 'Recommendation', description: 'Recommendation detail', http: {source: 'body'}},
+        returns: {arg: 'book', type: Recommendation, root: true}
+    });
 };
