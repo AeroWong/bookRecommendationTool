e3e0bcaa19aa19901fefc6a974df538672011687
Aero Wong <aerowongmanhon@gmail.com>
2016-05-25 10:38:14 +0800

    reference book by id

--- common/models/recommendation.js
+++ common/models/recommendation.js
@@ -4,7 +4,7 @@ var app = require('../../server/server.js');
 module.exports = function(Recommendation) {
     Recommendation.addRecommendation = function (recommendation, cb) {
         // recommendation object for testing
-        var recommendation = { bookTitle: 'Fuck That Shit',
+        var recommendation = { bookTitle: 'on writing welL',
                                authors: ['Enimen','Snoop Dogg'],
                                amazonPage: 'amazon.hiphop.com',
                                categories: ['Hip Hop'],
@@ -18,7 +18,23 @@ module.exports = function(Recommendation) {
             egghead = recommendation.egghead,
             src = recommendation.src,
             lowerCaseEgghead = egghead.toLowerCase(),
-            lowerCaseEggheadsInBookshelf = null;
+            lowerCaseEggheadsInBookshelf = null,
+            lowerCaseBookTitle = bookTitle.toLowerCase(),
+            lowerCaseBookTitlesWithIdInBookshelf = null,
+            lowerCaseBookTitlesInBookshelf = null,
+            bookId = null,
+            // recommendation obj to save
+            recommendationObj = { id: null,
+                                  src: null,
+                                  book_id: null,
+                                  egghead_id: null },
+            // book obj to save
+            bookObj = { id: null,
+                        title: null,
+                        authors: null,
+                        amazon_page: null,
+                        categories_id: null,
+                        egghead_id: null };
 
         // check if bookshelf has the egghead
         var hasEgghead = app.models.EggHead.find().then(function(eggheads){
@@ -29,7 +45,18 @@ module.exports = function(Recommendation) {
             }
         })
         // reference book id by title
-        
+        var getBookId = app.models.Book.find().then(function(books){
+            lowerCaseBookTitlesWithIdInBookshelf = reformBookshelfElements(books);
+            lowerCaseBookTitlesInBookshelf = turnBookshelfElementsToLowerCase(books);
+            // check if there is a duplicated book
+            lowerCaseBookTitlesWithIdInBookshelf.forEach(function(book){
+                if (lowerCaseBookTitle === book.title) {
+                    bookId = book.id;
+                } else {
+                    bookId = 'b-' + (books.length + 1);
+                }
+            })
+        })
         // check if there is a duplicated recommendation
 
         Promise.all([hasEgghead]);
@@ -53,5 +80,35 @@ module.exports = function(Recommendation) {
                 return elem.name.toLowerCase();
             }
         })
+    }
+    function reformBookshelfElements (elems) {
+        if (elems[0].id.charAt(0) === 'c') {
+            return elems.map(function(category){
+                var reformedCategory = {};
+                reformedCategory.name = category.name.toLowerCase();
+                reformedCategory.categories_id = category.id;
+                return reformedCategory;
+            })
+        } else if (elems[0].id.charAt(0) === 'e') {
+            return elems.map(function(egghead){
+                var reformedEgghead = {};
+                reformedEgghead.name = egghead.name.toLowerCase();
+                reformedEgghead.profile_pic = egghead.profile_pic;
+                reformedEgghead.site = egghead.site;
+                reformedEgghead.id = egghead.id;
+                return reformedEgghead;
+            })
+        } else if (elems[0].id.charAt(0) === 'b') {
+            return elems.map(function(book){
+                var reformedBook = {};
+                reformedBook.id = book.id;
+                reformedBook.title = book.title.toLowerCase();
+                reformedBook.authors = book.authors;
+                reformedBook.amazonPage = book.amazon_page;
+                reformedBook.categories = book.categories_id;
+                reformedBook.eggheads = book.eggheads_id;
+                return reformedBook;
+            })
+        }
     }
 };
