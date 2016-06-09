5e49db9a71575640846cc3bd89b520e789a7c1c8
Aero Wong <aerowongmanhon@gmail.com>
2016-06-09 11:17:15 +0800

    refactored getCategoryRecommendations endpoint

--- common/models/category.js
+++ common/models/category.js
@@ -3,26 +3,20 @@ var Promise = require('bluebird');
 var app = require('../../server/server.js');
 
 module.exports = function(Category) {
-    Category.getCategoryBooks = function(categoryAlias, cb) {
-        var categoryInfoObj = { name: null,
-                                bookCount: null,
-                                books: [] };
+    Category.getCategoryRecommendations = function(categoryAlias, cb) {
+        var categoryInfoObj = {};
         // get the category name and the number of book count
         var getRefromedBooks = Category.findOne({where: {alias: categoryAlias}})
         .then(function(category){
-            categoryInfoObj.name = category.name;
-            categoryInfoObj.bookCount = category.books_id.length;
+            categoryInfoObj.categoryName = category.name;
+            categoryInfoObj.recommendations = {};
+            categoryInfoObj.recommendations.count = category.books_id.length;
             return category.books_id;
         })
         .then(function(booksId){
             //get the books id and start to construct books
             return Promise.map(booksId, function(bookId){
-                var reformedBook = { id: null,
-                                     title: null,
-                                     alias: null,
-                                     authors: [],
-                                     amazonPage: null };
-
+                var reformedBook = { id: null };
                 reformedBook.id = bookId;
                 return reformedBook;
             })
@@ -58,14 +52,14 @@ module.exports = function(Category) {
                         reformedBook.title = book.title;
                         reformedBook.authors = book.authors;
                         reformedBook.alias = book.alias;
-                        reformedBook.recommendations = [];
+                        reformedBook.eggheads = [];
                     }
                 })
                 return reformedBook;
             })
 
             blankRecommendations = recommendations.map(function(recommendation){
-                var reformedRecommendation = { recommendations: [] };
+                var reformedRecommendation = {};
                 eggheads.forEach(function(egghead){
                     if (recommendation.egghead_id === egghead.id) {
                         reformedRecommendation.bookId = recommendation.book_id;
@@ -77,21 +71,24 @@ module.exports = function(Category) {
             uniqReformedRecommendations = _.uniqBy(blankRecommendations, 'bookId');
 
             reformedRecommendations = recommendations.map(function(recommendation){
-                var reformedRecommendation = {};
+                var reformedEgghead = {};
                 eggheads.forEach(function(egghead){
                     if (recommendation.egghead_id === egghead.id) {
-                        reformedRecommendation.bookId = recommendation.book_id;
-                        reformedRecommendation.eggheadName = egghead.name;
-                        reformedRecommendation.src = recommendation.src;
+                        reformedEgghead.bookId = recommendation.book_id;
+                        reformedEgghead.name = egghead.name;
+                        reformedEgghead.alias = egghead.alias;
+                        reformedEgghead.src = recommendation.src;
+                        reformedEgghead.srcTitle = recommendation.src_title;
                     }
                 })
-                return reformedRecommendation;
+                return reformedEgghead;
             })
 
             fullRecommendations = uniqReformedRecommendations.map(function(uniqRecommendation){
+                uniqRecommendation.eggheads = [];
                 reformedRecommendations.forEach(function(reformedRecommendation){
                     if (uniqRecommendation.bookId === reformedRecommendation.bookId) {
-                        uniqRecommendation.recommendations.push(reformedRecommendation);
+                        uniqRecommendation.eggheads.push(reformedRecommendation);
                     }
                 })
                 return uniqRecommendation;
@@ -100,14 +97,15 @@ module.exports = function(Category) {
             return fullBooks.map(function(book){
                 fullRecommendations.forEach(function(recommendation){
                     if (book.id === recommendation.bookId) {
-                        book.recommendations = recommendation.recommendations;
+                        book.eggheads = recommendation.eggheads;
                     }
                 })
                 return book;
             })
         })
         .then(function(books){
-            categoryInfoObj.books = books;
+            categoryInfoObj.recommendations.books = books;
+            console.log("rendering category '" + categoryInfoObj.categoryName + " book recommendations...");
             cb(null, categoryInfoObj);
         })
     }
@@ -132,9 +130,9 @@ module.exports = function(Category) {
             console.log(e);
         })
     }
-    Category.remoteMethod ('getCategoryBooks', {
+    Category.remoteMethod ('getCategoryRecommendations', {
         description: 'get a list of books from a selcted category',
-        http: {path: '/getCategoryBooks', verb: 'get', status: 200},
+        http: {path: '/getCategoryRecommendations', verb: 'get', status: 200},
         accepts: {arg: 'filter', type: 'string', description: "Filter defining fields, where, include, order, offset, and limit", http: {source: 'query'}},
         returns: {arg: 'category info and its books', type: Category, root: true}
     });
