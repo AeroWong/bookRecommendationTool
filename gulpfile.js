7c5fa0a507ea2a5c8d831f50d674dd964e8d958b
Aero Wong <aerowongmanhon@gmail.com>
2016-06-18 16:12:17 +0800

    use foundation front-end file locally

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    $    = require('gulp-load-plugins')(),
     minifyCSS = require('gulp-minify-css'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    sassPaths = [
+      'bower_components/foundation-sites/scss',
+      'bower_components/motion-ui/src'
+    ];
 
 // Defines path to source and assets
 var project = {
@@ -11,7 +16,14 @@ var project = {
 
 gulp.task('build-css', function(){
     return gulp.src(project.src + '/scss/**/*.scss')
-        .pipe(sass())
+        .pipe($.sass({
+          includePaths: sassPaths,
+          outputStyle: 'compressed' // if css compressed **file size**
+        })
+          .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+          browsers: ['last 2 versions', 'ie >= 9']
+        }))
         .pipe(cleanCSS())
         .pipe(minifyCSS())
         .pipe(gulp.dest(project.dest + '/css'));
