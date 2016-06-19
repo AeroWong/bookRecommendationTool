e04a38311b5fe425fb9fca2fde95add8e9863689
Aero Wong <aerowongmanhon@gmail.com>
2016-06-20 06:39:47 +0800

    building login

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -1,5 +1,28 @@
 (function() {
-    'use strict';
-    
+	'use strict';
+	var admin = {
+		init: function(){
+			admin.login();
+		},
+		login: function(){
+			$('#log-in-form .button').click(function(){
 
-})();
+				var obj = { email: $('#log-in-form #email').val(),
+							password: $('#log-in-form #password').val()};
+
+				$.ajax({
+					type: "POST",
+					url: ((window.location.hostname.indexOf('wisdomtrigger.com') > -1)? 'https://' : 'http://') + window.location.hostname + ':3000/login',
+					dataType: 'json',
+					data: obj
+				}).success(function(res){
+					console.log('success', res);
+				}).error(function(res){
+					console.log('error', res);
+				})
+			})
+
+		}
+	}
+	admin.init();
+}());
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next){
     res.render('pages/admin', {layout: 'admin'});
 })
 
+app.post('/login', function(req, res) {
+	console.log('---- ', obj);
+	app.models.User.login({
+	  	email: req.body.email,
+	  	password: req.body.password
+	}, 'user', function(err, token) {
+	  	if (err) {
+	    	res.render('response', { //render view named 'response.ejs'
+	      	title: 'Login failed',
+	      	content: err,
+	      	redirectTo: '/',
+	      	redirectToLinkText: 'Try again'
+	    });
+	    	return;
+	  	}
+
+	  	res.render('home', { //login user and render 'home' view
+	    	email: req.body.email,
+	   		accessToken: token.id
+	  	});
+	});
+});
+
 module.exports = router;
