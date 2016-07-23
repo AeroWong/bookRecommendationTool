1e74c307695521d0b465e752d232752df1f64748
Aero Wong <aerowongmanhon@gmail.com>
2016-07-23 19:03:05 +0800

    commented out automatically createing admin user when app inits

--- server/boot/user.js
+++ server/boot/user.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
 	var User = app.models.User;
-    User.create({email: 'dummy@email.com', password: 'foobar'}, function(err, userInstance) {
-	   console.log('One admin user was just created...');
-	});
+ //    User.create({email: 'dummy@email.com', password: 'foobar'}, function(err, userInstance) {
+	//    console.log('One admin user was just created...');
+	// });
 }
